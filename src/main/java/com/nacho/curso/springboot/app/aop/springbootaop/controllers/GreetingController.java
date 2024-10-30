e22b5ef1cb4e10a33f77414a4b520d06cee34abd
borparnax <ig.bort@gmail.com>
2024-10-30 12:29:58 +0100

    Initial commit

---
+++ src/main/java/com/nacho/curso/springboot/app/aop/springbootaop/controllers/GreetingController.java
@@ -0,0 +1,27 @@
+package com.nacho.curso.springboot.app.aop.springbootaop.controllers;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nacho.curso.springboot.app.aop.springbootaop.services.GreetingService;
+
+@RestController
+public class GreetingController {
+    
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/greeting")
+    public ResponseEntity<?> greeting() {
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greetingService.sayHello("Luis", "Hola que tal!")));
+    }
+    @GetMapping("/greeting-error")
+    public ResponseEntity<?> greetingError() {
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greetingService.sayHelloError("Luis", "Hola que tal!")));
+    }
+
+}
---
+++ src/main/java/com/nacho/curso/springboot/app/aop/springbootaop/services/GreetingService.java
@@ -0,0 +1,6 @@
+package com.nacho.curso.springboot.app.aop.springbootaop.services;
+
+public interface GreetingService {
+    String sayHello(String person, String phrase);
+    String sayHelloError(String person, String phrase);
+}
